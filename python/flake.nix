b41c2a4717509628fbba9337dace8216f9b6f34a
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-07 19:48:51 -0400

    Formatted using alejandra

--- python/flake.nix
+++ python/flake.nix
@@ -5,19 +5,21 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-    let
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forEachSystem = nixpkgs.lib.genAttrs systems;
-      pkgs = forEachSystem (system: nixpkgs.legacyPackages.${system});
-    in {
-      devShells = forEachSystem (system: {
-        default = pkgs.${system}.mkShell {
-          packages = with pkgs.${system}; [
-            python3
-            poetry
-          ];
-        };
-      });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system: nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forEachSystem (system: {
+      default = pkgs.${system}.mkShell {
+        packages = with pkgs.${system}; [
+          python3
+          poetry
+        ];
+      };
+    });
+  };
 }
