e3a77f529f451da6f798fd0bad81a6abd941e07b
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-08 17:12:50 -0400

    Added Python app to main flake

--- flake.nix
+++ flake.nix
@@ -1,1 +1,23 @@
+{
+  description = "A very basic flake";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    python-app.url = "path:./python";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    python-app,
+  }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+  in {
+    apps = forEachSystem (system: {
+      python = python-app.apps.${system}.default;
+    });
+  };
+}
