2c1fc44eaa4d2502c25080d2391d83f6e3b1afe0
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-08 17:19:01 -0400

    Added Rust as apps

--- flake.nix
+++ flake.nix
@@ -5,24 +5,26 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     python-app.url = "path:./python";
+    rust-app.url = "path:./rust";
   };
 
   outputs = {
     self,
     nixpkgs,
     python-app,
+    rust-app,
   }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgs = forEachSystem (system: import nixpkgs {inherit system;});
   in {
     apps = forEachSystem (system: {
-      default = { 
+      default = {
         type = "app";
         program = "${pkgs.${system}.hello}/bin/hello";
       };
       python = python-app.apps.${system}.default;
-
+      rust = rust-app.apps.${system}.default;
     });
   };
 }
