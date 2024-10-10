84837c2f00faabf31d6617a7c7585894df94ecd0
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-10 18:23:24 -0400

    Added C and flake

--- C/flake.nix
+++ C/flake.nix
@@ -5,11 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }: let
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+  in{
+    devShells = forEachSystem (system: {
+      default = pkgs.${system}.mkShell.override {
+        packages = with pkgs.${system}; [
+          clang-tools
+          gtest
+        ];
+      };
+    });
   };
 }
