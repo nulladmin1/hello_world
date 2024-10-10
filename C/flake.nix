a51701de3d5ae38ab7338ee1823c68cce7df528b
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-10 18:25:58 -0400

    Fixed override error

--- C/flake.nix
+++ C/flake.nix
@@ -5,13 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgs = forEachSystem (system: import nixpkgs {inherit system;});
-  in{
+  in {
     devShells = forEachSystem (system: {
-      default = pkgs.${system}.mkShell.override {
+      default = pkgs.${system}.mkShell.override {} {
         packages = with pkgs.${system}; [
           clang-tools
           gtest
