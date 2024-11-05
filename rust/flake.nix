d6036193eac8d967b3817e0b6371d7e66f2b13ff
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-04 22:52:19 -0500

    Switched to using fenix + naersk

--- rust/flake.nix
+++ rust/flake.nix
@@ -3,31 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    naersk,
+    fenix,
+    ...
   }: let
     systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
-    pkgs = forEachSystem (system: import nixpkgs {inherit system;});
+    pkgs = forEachSystem (system: 
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          fenix.overlays.default
+        ];
+    });
+    rust-toolchain = forEachSystem (system: pkgs.${system}.fenix.complete);
   in {
     devShells = forEachSystem (system: {
       default = pkgs.${system}.mkShell {
-        packages = with pkgs.${system}; [
+        packages = with rust-toolchain.${system}; [
           cargo
           rustc
           rust-analyzer
+          rustfmt
+          clippy
         ];
+        RUST_SRC_PATH = "${rust-toolchain.${system}.rust-src}/lib/rustlib/src/rust/library";
       };
     });
 
     packages = forEachSystem (system: {
-      default = pkgs.${system}.rustPlatform.buildRustPackage {
-        pname = "hello-world";
-        version = "0.1";
-        cargoLock.lockFile = ./Cargo.lock;
+      default = (pkgs.${system}.callPackage naersk {
+        cargo = rust-toolchain.${system}.cargo;
+        rustc = rust-toolchain.${system}.rustc;
+      }).buildPackage {
         src = ./.;
       };
     });
